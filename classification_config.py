1f0d8a1631a790e35c54809e87eac37ac00780d6
hammb <benyhamm@gmail.com>
2022-05-30 11:33:58 +0200

    add BATCH_SIZE

---
+++ classification_config.py
@@ -0,0 +1,7 @@
+import os
+
+TASK = "space"
+FOLD = 0
+BATCH_SIZE=32
+TRAIN_DIR = os.path.join("/home/AD/b556m/data/classification_space/classification_space_preprocessed_b0", TASK, "train/all_samples")
+TEST_DIR = os.path.join("/home/AD/b556m/data/classification_space/classification_space_preprocessed_b0", TASK, "/test/all_samples")
